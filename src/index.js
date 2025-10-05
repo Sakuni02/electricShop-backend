2ff6fce673ef688f4bd16e5bf6202e8833daabb5
Sakuni Sandakalmi <masandakalmisakuni@gmail.com>
2025-10-05 16:33:47 +0530

    electroShop-backend second commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import categoryRouter from "./api/category.js";
 import { connectDB } from "./infrastructure/db/index.js";
 import reviewRouter from "./api/review.js";
 import globalErrorHandlingMiddleware from "./api/middleware/global-error-handling-middleware.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api/products", productRouter);
 app.use("/api/categories", categoryRouter);
