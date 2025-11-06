b79d2d6d5424b1232f33e1cc67ae84c4eade10cb
Sakuni Sandakalmi <masandakalmisakuni@gmail.com>
2025-11-06 19:42:11 +0530

    cloudflare R2 Object Storage

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,22 +1,30 @@
 import express from "express";
 import {
-    createProduct,
     getAllProducts,
-    deleteProductById,
+    createProduct,
     getProductById,
     updateProductById,
+    deleteProductById,
+    uploadProductImage,
 } from "../application/product";
 import isAuthenticated from "./middleware/authentication-middleware";
 import { isAdmin } from "./middleware/authorization-middleware";
 
 const productRouter = express.Router();
 
-productRouter.route("/").get(getAllProducts).post(isAuthenticated, isAdmin, createProduct);
+productRouter
+    .route("/")
+    .get(getAllProducts)
+    .post(isAuthenticated, isAdmin, createProduct);
 
 productRouter
     .route("/:id")
     .get(getProductById)
     .put(updateProductById)
     .delete(deleteProductById);
+
+productRouter
+    .route("/images")
+    .post(uploadProductImage);
 
 export default productRouter;
